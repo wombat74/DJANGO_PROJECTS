7a2d0b8c6e4777d73bf577ec0856b44af428c642
Eddie Gonzalez <eddie_gonzalez@Eddies-Air.fios-router.home>
2021-11-17 20:22:48 -0800

    added book_list.html and updated views and urls

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """View function for home page of site."""
@@ -31,3 +32,7 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+
